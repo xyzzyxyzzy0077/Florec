f4f0c101e8ce0a3412e40cd47b1f95a20bc76104
Ethan <sunnysun9312@gmail.com>
2017-10-25 16:13:16 +1030

    Cleaned mainpage after extracted map.js

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -18,7 +18,7 @@ import {
   Text,
   StyleProvider
 } from 'native-base';
-import MapView from 'react-native-maps'
+
 import Map from '../components/Map.js'
 import getTheme from '../native-base-theme/components';
 import platform from '../native-base-theme/variables/platform';
@@ -31,49 +31,13 @@ export default class Main extends Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      region: {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0022,
-        longitudeDelta: 0.0048,
-      },
-    }
   }
 
-  componentDidMount() {
-    this.getAndUpdateLocation()
-  }
-
-  getAndUpdateLocation() {
-    navigator.geolocation.getCurrentPosition(
-      (data) => {
-        const region = {
-          latitude: data.coords.latitude,
-          longitude: data.coords.longitude,
-          latitudeDelta: 0.0022,
-          longitudeDelta: 0.0048,
-        };
-
-        this.setState({
-          region
-        });
-      },
-      (err) => {
-        console.log('err', err);
-      },
-      {}
-    );
-  }
-
-  onRegionChangeComplete(region) {
-    this.setState({ region });
-  }
 
   render() {
     return (
       <Container style={styles.container}>
+
         <Map/>
 
         <StyleProvider style={getTheme(platform)}>
@@ -99,17 +63,6 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#f2f2f2',
     flex: 1,
-  },
-  map: {
-    flex:1
-  },
-  locateButton: {
-    position: 'absolute',
-    bottom: Dimensions.get("window").height * 0.11,
-    right: Dimensions.get("window").height * 0.05,
-    height: 53,
-    borderRadius: 100,
-    backgroundColor: 'rgba(0,0,0,0.5)'
   },
   footer: {
     height: Dimensions.get("window").height * 0.06,
